57944088b02b87cd72141ade1da0eaed9a01e45c
Malik Albeik <abd.beik@me.com>
2018-04-16 11:16:23 +0300

    Added is_rest function to helpers.c in music, the thrid problem set.

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -19,5 +19,14 @@ int frequency(string note)
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-    // TODO
+    // if the first char of s is \0 than the given string is empty return true.
+    if (s[0] == '\0')
+    {
+	return (true);
+    }
+    // else the string is not a rest return false.
+    else
+    {
+	return (false);
+    }
 }

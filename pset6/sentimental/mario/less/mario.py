e45f970327d16308327a1096ace33515b07146c8
Malik Albeik <abd.beik@me.com>
2018-05-13 01:33:27 +0300

    Added mario.py the less version.

---
+++ pset6/sentimental/mario/less/mario.py
@@ -0,0 +1,18 @@
+from cs50 import get_int
+
+# ask for the number till the user inputs a valid number.
+while True:
+    number = get_int("Enter an int: ")
+    if (number < 24 and number > - 1):
+        break
+
+# Do this on every line
+for i in range(number):
+    # prints the initial spaces to make it look like a half pyramid.
+    for k in range(number - i - 1):
+        print(" ", end="")
+    print("#", end="")
+    for j in range(i + 1):
+        print("#", end="")
+    # prints a line at the end of every line.
+    print("\n", end="")
